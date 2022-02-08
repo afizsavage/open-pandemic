87f4be44671aa2aec3f2d5e6b55b68710281ee0b
Afiz Savage <savageafiz9@gmail.com>
2022-02-08 11:11:15 +0000

    Create page routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Details from './components/Details/index';
+import Home from './components/Home/index';
+
 import './App.css';
 
-const App = () => (<div>App</div>);
+const App = () => (
+  <>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  </>
+);
 
 export default App;
