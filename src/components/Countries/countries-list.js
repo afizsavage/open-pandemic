77e0ff1017299f4bebe7f53fb32ccdde79381c5c
Afiz Savage <savageafiz9@gmail.com>
2022-02-10 08:50:14 +0000

    Fetched data when click on a country item

--- src/components/Countries/countries-list.js
+++ src/components/Countries/countries-list.js
@@ -1,8 +1,22 @@
-import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { createAction, VIEW_COUNTRY_DETAILS } from '../../redux/countries';
+
+const dispatchAction = async (country, dispatch) => {
+  dispatch(createAction(VIEW_COUNTRY_DETAILS, country));
+};
+
+const fetchCountryDetails = async () => {
+  const data = axios.get(
+    'https://api.covid19tracking.narrativa.com/api/2020-03-22/country/cuba',
+  );
+  return data;
+};
 
 const ListOfCountries = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const countriesState = useSelector((state) => state.countries);
 
   return (
@@ -10,10 +24,24 @@ const ListOfCountries = () => {
       <ul>
         {countriesState.searching === false && countriesState.searchResult.length === 0
           ? countriesState.fetched.map((country) => (
-            <li key={country.id}><a href onClick={() => navigate('/details')}>{country.name}</a></li>
-
-          )) : countriesState.searchResult.map((country) => (
-            <li key={country.id}><a href onClick={() => navigate('/details')}>{country.name}</a></li>
+            <li key={country.id}>
+              <a
+                href
+                onClick={() => fetchCountryDetails().then((country) => dispatchAction(country, dispatch).then(navigate('/details')))}
+              >
+                {country.name}
+              </a>
+            </li>
+          ))
+          : countriesState.searchResult.map((country) => (
+            <li key={country.id}>
+              <a
+                href
+                onClick={() => fetchCountryDetails().then((country) => dispatchAction(country, dispatch).then(navigate('/details')))}
+              >
+                {country.name}
+              </a>
+            </li>
           ))}
       </ul>
     </div>
--- src/redux/countries/index.js
+++ src/redux/countries/index.js
@@ -1,7 +1,10 @@
 export const ADD_COUNTRY = 'open-pandemic/countries/addCountry';
 export const SEARCH_COUNTRY = 'open-pandemic/countries/searchCountry';
+export const VIEW_COUNTRY_DETAILS = 'open-pandemic/countries/viewCountryDetails';
 
-const initialState = { fetched: [], searchResult: [], searching: false };
+const initialState = {
+  fetched: [], searchResult: [], searching: false, currentCountry: null,
+};
 
 export const createAction = (actionType, payload) => ({
   type: actionType,
@@ -18,12 +21,19 @@ const searchCountry = (state, value) => {
   return { ...state, searchResult: filteredCountries, searching: true };
 };
 
+const viewCountryDetails = (state, country) => {
+  const newState = { ...state, currentCountry: country };
+  return newState;
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_COUNTRY:
       return { ...state, fetched: [...state.fetched, action.payload] };
     case SEARCH_COUNTRY:
       return searchCountry(state, action.payload);
+    case VIEW_COUNTRY_DETAILS:
+      return viewCountryDetails(state, action.payload);
     default:
       return state;
   }
