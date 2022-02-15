593c535667873120b17ace017efa7d07e53e062b
Afiz Savage <savageafiz9@gmail.com>
2022-02-15 13:39:49 +0000

    Conditionally render searchbar

--- src/components/Countries/hero-section.js
+++ src/components/Countries/hero-section.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const HeroSection = () => {
   const [cases, setcases] = useState('');
+  const [name, setname] = useState('');
+  const location = useLocation();
+  const state = useSelector((state) => state.countries);
+
+  const home = '/';
 
   useEffect(() => {
-    axios
-      .get('https://api.covid19tracking.narrativa.com/api/2021-12-11')
-      .then((cases) => setcases(cases.data.total.today_confirmed));
+    if (location.pathname === home) {
+      axios
+        .get('https://api.covid19tracking.narrativa.com/api/2021-12-11')
+        .then((cases) => setcases(cases.data.total.today_confirmed));
+    } else {
+      const countryName = Object.keys(state.currentCountry)[0];
+      const countryData = Object.values(state.currentCountry)[0];
+      setcases(countryData.today_confirmed);
+      setname(countryName);
+    }
   }, []);
 
   return (
     <section id="hero" className="b-secondary full-width">
       <div className="full-height flex center-y flex-end">
         <div id="stats">
-          <h1 className="white-text">Global</h1>
+          <h1 className="white-text">{location.pathname === home ? 'Global' : name}</h1>
           <span className="white-text">{`${cases} Cases`}</span>
         </div>
       </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,22 @@
 import { ImMic } from 'react-icons/im';
 import { MdSettings } from 'react-icons/md';
+import { useLocation } from 'react-router-dom';
 
 import SearchBar from './search-bar';
 
-const Header = () => (
-  <header className="b-primary flex white-text center-y space-between">
-    <SearchBar />
-    <span>Cases</span>
-    <span>
-      <ImMic className="head-icon" />
-      <MdSettings className="head-icon" />
-    </span>
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header className="b-primary flex white-text center-y space-between">
+      {location.pathname === '/' && <SearchBar />}
+      <span>Cases</span>
+      <span>
+        <ImMic className="head-icon" />
+        <MdSettings className="head-icon" />
+      </span>
+    </header>
+  );
+};
 
 export default Header;
